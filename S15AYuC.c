fd51dc1eb8bfbfb7c66e7849d2d1817579d5aac1
Chrisander Yu <161602611+ChrisanderJ@users.noreply.github.com>
2025-10-24 21:43:15 +0800

    Update S15AYuC.c

--- S15AYuC.c
+++ S15AYuC.c
@@ -1,11 +1,3 @@
-/*********************************************************************************************************
-This is to certify that this project is my own work, based on my personal efforts in studying and applying the concepts
-learned. I have constructed the functions and their respective algorithms and corresponding code by myself. The
-program was run, tested, and debugged by my own efforts. I further certify that I have not copied in part or whole or
-otherwise plagiarized the work of other students and/or persons.
-<Yu, Chrisander Jervin C.>, DLSU ID# <12318493>
-*********************************************************************************************************/
-
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>  // for uppercasing and lowercasing
